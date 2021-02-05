96ecd6529787ed8dd6b4204b8bf5ec6470e7e2ed
azizrosyid <azizrosyid783@gmail.com>
2021-02-05 11:32:17 +0700

    menambahkan jawaban

--- Permutations (unfinished)/main.py
+++ Permutations (unfinished)/main.py
@@ -1,6 +1,76 @@
-def permutations(string):
-    for i in range(len(string)):
-        print(string[i])
+# def permutations(string):
+#     count_str = len(string)
+#     loop = 0
 
 
-permutations('aabb')
+# permutations('aabb')
+
+# Python program to print all permutations using
+# Heap's algorithm
+ 
+# Prints the array
+ 
+ 
+def printArr(a, n):
+    for i in range(n):
+        print(a[i], end=" ")
+    print()
+ 
+# Generating permutation using Heap Algorithm
+ 
+ 
+def heapPermutation(a, size, n):
+ 
+    # if size becomes 1 then prints the obtained
+    # permutation
+    if (size == 1):
+        printArr(a, n)
+        return
+ 
+    for i in range(size):
+        if (size == 1):
+            printArr(a, n)
+            return
+        
+        for i in range(size):
+            if (size == 1):
+                printArr(a, n)
+                return
+        
+            for i in range(size):
+                heapPermutation(a, size-1, n)
+        
+                # if size is odd, swap 0th i.e (first) 
+                # and (size-1)th i.e (last) element
+                # else If size is even, swap ith 
+                # and (size-1)th i.e (last) element
+                if size & 1:
+                    a[0], a[size-1] = a[size-1], a[0]
+                else:
+                    a[i], a[size-1] = a[size-1], a[i]
+    
+            # if size is odd, swap 0th i.e (first) 
+            # and (size-1)th i.e (last) element
+            # else If size is even, swap ith 
+            # and (size-1)th i.e (last) element
+            if size & 1:
+                a[0], a[size-1] = a[size-1], a[0]
+            else:
+                a[i], a[size-1] = a[size-1], a[i]
+ 
+        # if size is odd, swap 0th i.e (first) 
+        # and (size-1)th i.e (last) element
+        # else If size is even, swap ith 
+        # and (size-1)th i.e (last) element
+        if size & 1:
+            a[0], a[size-1] = a[size-1], a[0]
+        else:
+            a[i], a[size-1] = a[size-1], a[i]
+ 
+ 
+# Driver code
+a = [char for char in "aabb"]
+n = len(a)
+heapPermutation(a, n, n)
+ 
+# This code is contributed by ankush_953
--- URL shortener (unfinished)/main.py
+++ URL shortener (unfinished)/main.py
@@ -1,7 +1,41 @@
 # https://www.codewars.com/kata/5fee4559135609002c1a1841/train/python
+import re
+db = {}
+
+
+def clean_url(url):
+    regex = "^(?:https?:\\/\\/)?(?:www\\.)?"
+    url = re.sub(regex, "", url)
+    url = re.sub("[^a-z]+", "", url)
+    url = url.replace(".", "")
+    return url
+
+def generate_id(url):
+    url = clean_url(url) * 10
+    gap = len(url) // (len(url) // 5)
+    result = ""
+    for i,v in enumerate(range(gap+1,len(url),gap)):
+        result += url[v]
+        if i == 3:
+            break
+    return result
 
 def url_shortener(long_url):
+    global db
+    short_url = "short.ly/"+generate_id(long_url)
+    db[short_url] = long_url
     return short_url
 
 def url_redirector(short_url):
-    return long_url
+    return db[short_url]
+
+
+short_url1 = url_shortener(
+    "https://www.codewars.com/kata/5ef9c85dc41b4e000f9a645f")
+short_url2 = url_shortener(
+    "https://www.codewars.com/kata/5ef9c85dc41b4e000f9a645f")
+print(db)
+print(short_url1)
+print(short_url2)
+# "https://www.codewars.com/kata/5ef9c85dc41b4e000f9a645f"
+url_redirector(short_url1)
